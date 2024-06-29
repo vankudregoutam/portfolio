d3d50bfad5935d85ede663aa71ed17bf15e4c826
unknown <vankudregoutam15@gmail.com>
2024-06-29 19:36:00 +0530

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
+require('dotenv').config({ path: '.env.local' });
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb+srv://vankudregoutam15:TdyRJgPiVCeUz271@cluster0.3yp5gis.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = process.env.MONGOURL
 // const mongoURI = 'mongodb://localhost:27017'
+// console.log(mongoURI);
 
-const connectToMongo =  () => {
-    mongoose.connect(mongoURI)
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURI)
     const db = mongoose.connection;
     db.on('error', (error) => {
         console.error('Mongo connection error: ', error);
--- backend/routes/message.js
+++ backend/routes/message.js
@@ -4,10 +4,8 @@ const Message = require('../models/Messages')
 
 // Route 1: Adding the Message using: POST '/api/message/addMessage'
 router.post('/addMessage', async (req, res) => {
-    console.log(req.body);
     try {
         const { name, email, subject, message } = req.body
-        console.log(name, email, subject, message);
         const messages = new Message({
             name, email, subject, message
         })
