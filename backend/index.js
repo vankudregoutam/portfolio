7bf8ae09f1406df17ec2950824a0620ffb5c8547
Goutam Vankudre <79298277+vankudregoutam@users.noreply.github.com>
2024-07-01 13:54:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ connectToMongo()
 const app = express()
 
 app.use(cors({
-    origin: 'https://goutam-vankudre-portfolio.vercel.app'
+    origin: 'https://goutam-vankudre-portfolio.vercel.app',
+    methods: ['GET', 'POST', 'OPTIONS'], // Specify the allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
 }))
 
 app.use(express.json())
